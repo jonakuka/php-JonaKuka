f3d0fcd2bb52f47b543c230df71be7fdcf3aa37e
jonakuka <jonakuka123@gmail.com>
2025-03-04 20:42:52 +0100

    lesson 1

---
+++ lesson1/first.php
@@ -0,0 +1,21 @@
+<?php 
+$school = 'Digital School';
+$numri1 = 50;
+$numri2 = 120;
+
+$numri3 = 18;
+$numri4 = 4;
+
+echo $numri3 % $numri4;
+
+//echo $numri1 + $numri2;//
+//echo "<br>";//
+//echo $numri1 * $numri2;//
+//echo "<br>";//
+//echo $numri1 / $numri2;//
+
+//echo "i  /$3  love $school"// 
+
+
+//echo "Hello World"//
+?>
---
+++ lesson1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> another type of php</h1>
+    
+</body>
+<?php
+echo "this is my first php" 
+?>
+</html>

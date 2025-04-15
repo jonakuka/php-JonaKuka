cf8b9107402a08802a49ca882e928c4905e30c96
jonakuka <jonakuka123@gmail.com>
2025-04-15 20:44:27 +0200

    day 11

---
+++ day 11/dashboard.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <?php
+
+    include_once("confing.php");
+    $sql = "select * from users";
+    $getUsers = $conn->prepare($sql);
+
+    $getUsers->execute();
+
+    $users=$getUsers->fetchAll();
+    
+?>
+
+<table>
+    <thead>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>SURNAME</th>
+        <th>EMAIL</th>
+    </thead>
+
+    <tbody>
+    <?php 
+    foreach($users as $user){
+        ?>
+        <tr>
+            <td><?= $user['id']?></td>
+            <td><?= $user['name']?></td>
+            <td><?= $user['surname']?></td>
+            <td><?= $user['email']?></td>
+            <td> <?="<a href='delete.php?id=$user[id]'>Delete</a>"?></td>
+
+        </tr>
+        <?php } ?>
+    
+    
+</tbody>
+</table>
+<a href="index.php">go to indeksss</a>
+
+
+
+    
+</body>
+</html>
